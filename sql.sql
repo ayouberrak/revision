6febe6f5423500e8875398924427658de4de708b
ayouberrak <ayouberrak30@gmail.com>
2026-01-30 09:46:04 +0100

    add sql 2

--- sql.sql
+++ sql.sql
@@ -11,3 +11,10 @@ VAlUES('banana',21,10) , ('pomme',10 , 22) , ('monge',2,0);
 UPDATE Produits SET prix = prix + prix *10 /100 WHERE id = 1;
 
 delete Produits WHERE stok = 0
+
+
+-- jour 2 
+select Count(*) as total from commandes
+select Avg(prix) as moyenne from Produits
+select Max(montant) as plus_chere from produit
+
